04fd8ea99b96efc3a31e3ac438619dd2d16fe77e
Wiola Szczepanik <wiolaszcz@gmail.com>
2017-10-25 12:56:41 +0200

    Controller - save error by lenght input

--- controller.py
+++ controller.py
@@ -11,11 +11,24 @@ class Controller:
     def add_item_to_app(self):
         """ Method adds new task to dictionary"""
 
-        name_for_task = input("Write name for a task: ")
-        description_for_task = input("Write description for a task: ")
+        while True:
+            name_for_task = input("Write name for a task: ")
+            if len(name_for_task) > 20:
+                print("Too long name. Write again")
+                continue
+            else:
+                break
+
+        while True:
+            description_for_task = input("Write description for a task: ")
+            if len(description_for_task) > 150:
+                print("Too long descriptopn. Write again")
+                continue
+            else:
+                break
+
         item_data = Model.todo_items
         item_data.update({name_for_task:description_for_task})
-
         print(item_data)
 
     def modify_item():
@@ -26,7 +39,8 @@ class Controller:
 
     def mark_item_as_done():
         pass
-
-
+# x = Controller()
+# x.add_item_to_app()
+# print(x)
 
 # task = Controller.add_item_to_app("lalal", "dhashdjashd")
--- model.py
+++ model.py
@@ -2,4 +2,4 @@ class Model:
 
     todo_items = {}
     # def __init__(self):
-    #     self.todo_items = []
+    #     self.todo_items = {}
